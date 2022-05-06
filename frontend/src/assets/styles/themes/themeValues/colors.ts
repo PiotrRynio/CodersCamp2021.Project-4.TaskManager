58f4bb277164f3dd072bf72b2be1a6491717e0cb
Agnieszka Urbanowicz <46648993+axseinga@users.noreply.github.com>
2022-05-06 23:22:09 +0100

    feat: add button component (#10)
    * feat: add button component

--- frontend/src/assets/styles/themes/themeValues/colors.ts
+++ frontend/src/assets/styles/themes/themeValues/colors.ts
@@ -2,10 +2,20 @@ export type Colors = {
   primary: string;
   secondary: string;
   regularText: string;
+  activePrimary: string;
+  hoverPrimary: string;
+  activeSecondary: string;
+  hoverSecondary: string;
+  disabled: string;
 };
 
 export const colors: Colors = {
   primary: '#4eadc5',
   secondary: '#648bb2',
-  regularText: '#000'
+  regularText: '#000',
+  activePrimary: '#0082a3',
+  hoverPrimary: '#20f7e9',
+  activeSecondary: '#EDF5E1',
+  hoverSecondary: '#8EE4AF',
+  disabled: '#ededed',
 };
