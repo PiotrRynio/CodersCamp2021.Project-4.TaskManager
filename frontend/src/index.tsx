8fac117c684c4b3568419978af37e41be3c03f18
Kos Krzysztof <krzysztof.kos33@gmail.com>
2022-05-10 03:45:33 +0200

    (feat) Add redux store

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,16 +4,20 @@ import { App } from 'views';
 import { AppProviders } from './providers/AppProviders';
 import { BrowserRouter } from 'react-router-dom';
 import { PageTitleProvider } from './providers/PageTitleProvider';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppProviders>
-        <PageTitleProvider>
-          <App />
-        </PageTitleProvider>
-      </AppProviders>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppProviders>
+          <PageTitleProvider>
+            <App />
+          </PageTitleProvider>
+        </AppProviders>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
