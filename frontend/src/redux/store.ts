b3395af21162dd42704bc0fa6adb2f507738e5fe
Kos Krzysztof <krzysztof.kos33@gmail.com>
2022-05-16 22:17:35 +0200

    save file

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import { todoTaskSlice } from './reducers/TodoTasksSlice';
+import { todoTaskSlice } from './reducers/TodoTasksSlice';
 
 export const store = configureStore({
   reducer: {},
