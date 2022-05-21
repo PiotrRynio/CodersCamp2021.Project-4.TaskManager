5b837b21863f0e7a1345d6f850344f04ba76e156
Hanna Kozak <hannawkozak@gmail.com>
2022-05-21 21:33:49 +0100

    feat(frontend): Add modal (#28)
    * feat: Modal
    
    * changes after PR

---
+++ frontend/src/components/Modal/Modal.tsx
@@ -0,0 +1,34 @@
+import { StyledModal } from './Modal.styles';
+
+export interface ModalProps {
+  title?: string;
+  isVisible: boolean;
+  onCancel: () => void;
+  onSubmit: (e: React.SyntheticEvent) => void;
+  submitButtonLabel?: string;
+  cancelButtonLabel?: string;
+  children: React.ReactNode;
+}
+
+export const Modal = ({
+  title,
+  isVisible,
+  onCancel,
+  onSubmit,
+  submitButtonLabel,
+  cancelButtonLabel,
+  children,
+}: ModalProps) => {
+  return (
+    <StyledModal
+      title={title}
+      visible={isVisible}
+      okText={submitButtonLabel ?? 'submit'}
+      cancelText={cancelButtonLabel ?? 'cancel'}
+      onOk={(e) => onSubmit(e)}
+      onCancel={() => onCancel()}
+    >
+      {children}
+    </StyledModal>
+  );
+};
