bcedf2a730d6340858d8a6796f84ea583d65b015
Krzysztof Kos <krzysztof.kos33@gmail.com>
2022-05-22 21:23:37 +0200

    rename file

--- frontend/src/views/TodoList/TodoList.tsx
+++ frontend/src/views/TodoList/TodoList.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { usePageTitle } from 'providers/PageTitleProvider';
 import { TodoTaskList } from 'components/TodoTaskList/TodoTaskList';
-import { todoTaskListMock } from 'mock-data/TodoTaskList/mock-1';
+import { todoTaskListMock } from 'mock-data/TodoTaskList/todoTaskListMock';
 
 export const TodoList = () => {
   const { setTitle } = usePageTitle();
