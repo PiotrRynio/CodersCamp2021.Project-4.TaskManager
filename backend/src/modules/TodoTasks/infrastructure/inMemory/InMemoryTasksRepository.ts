6e508ed646715ea34fc2b5f417a0bbb98f320bdf
sylcym <prusak.syl@gmail.com>
2022-05-23 20:26:45 +0200

    Update task handling

--- backend/src/modules/TodoTasks/infrastructure/inMemory/InMemoryTasksRepository.ts
+++ backend/src/modules/TodoTasks/infrastructure/inMemory/InMemoryTasksRepository.ts
@@ -4,11 +4,11 @@ import { Task } from '../../domain/Task';
 export class InMemoryTasksRepository implements TaskRepository {
   private readonly entities: { [id: string]: Task } = {};
 
-  async add(newTask: Task): Promise<void> {
+  async addTask(newTask: Task): Promise<void> {
     this.entities[newTask.taskId] = newTask;
   }
 
-  findAll(): Promise<Task[]> {
+  findAllTasks(): Promise<Task[]> {
     return Promise.resolve(Object.keys(this.entities).map((id) => this.entities[id]));
   }
 }
