3593be41a1fbf244fe0ffead375a508d4382d53a
sylcym <84134018+sylcym@users.noreply.github.com>
2022-05-24 21:25:06 +0200

    feat(be-tasks): update task module (#35)

--- backend/src/modules/TodoTasks/application/AddTaskCommand.ts
+++ backend/src/modules/TodoTasks/application/AddTaskCommand.ts
@@ -1,9 +1,13 @@
 export class AddTaskCommand {
   readonly taskName: string;
   readonly userId: string;
+  readonly isDone: boolean;
+  readonly isImportant: boolean;
 
-  constructor(props: { taskName: string; userId: string }) {
+  constructor(props: { taskName: string; userId: string; isDone: boolean; isImportant: boolean }) {
     this.taskName = props.taskName;
     this.userId = props.userId;
+    this.isDone = props.isDone;
+    this.isImportant = props.isImportant;
   }
 }
--- backend/src/modules/TodoTasks/domain/events/NewTaskWasAddedEvent.ts
+++ backend/src/modules/TodoTasks/domain/events/NewTaskWasAddedEvent.ts
@@ -5,11 +5,22 @@ export class NewTaskWasAddedEvent implements DomainEvent {
   readonly userId: string;
   readonly taskId: string;
   readonly taskName: string;
+  readonly isDone: boolean;
+  readonly isImportant: boolean;
 
-  constructor(props: { occurredAt: Date; userId: string; taskName: string; taskId: string }) {
+  constructor(props: {
+    occurredAt: Date;
+    userId: string;
+    taskName: string;
+    taskId: string;
+    isDone: boolean;
+    isImportant: boolean;
+  }) {
     this.occurredAt = props.occurredAt;
     this.userId = props.userId;
     this.taskName = props.taskName;
     this.taskId = props.taskId;
+    this.isDone = props.isDone;
+    this.isImportant = props.isDone;
   }
 }
